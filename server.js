5072242a36d452666d6d8893bb275019072c0920
Niharika Kanakala <niharika.k@aithinkers.com>
2022-05-11 18:09:23 +0530

    retrieving data from mongo db

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
-
-//Allow all requests from all domains & localhost
+const mongoose = require('mongoose');
+//Allow all requests from all domains & localhost//2OFL5PrOw2ByKnwD
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
@@ -17,13 +17,35 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 //Todos => deleted userId
 
-app.get('/todos', async (req, res) => {
+const TodoSchema = new mongoose.Schema (
+    {
+        userId: Number,
+        id: Number,
+        title: String,
+        completed: Boolean
+    }
+);
+
+const Todo  = mongoose.model('to-do', TodoSchema);
+
+
+
+app.get('/sync/with/mongo', async (req, res, next) => {
+    // const id = req.body.id;
+    // const title = req.body.title;
     try {
         const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos');
-        const desiredData = data.map(todo => {
+        const desiredData = await Promise.all(data.map(async todo => {
             delete todo.userId;
             return todo;
-        })
+        }));
+        await Todo.bulkWrite(desiredData.map(todo => ({
+            updateOne: {
+                filter: {id: todo.id},
+                update: todo,
+                upsert: true,
+            }
+        })))
         res.status(200).send(desiredData);
     } catch (error) {
         console.log('errror', error);
@@ -31,7 +53,32 @@ app.get('/todos', async (req, res) => {
     }
 });
 
+//retrieving data from mongo db to show all todos
+app.get('/todos', async(req, res) => {
+    try {
+        const data = await Todo.find({});
 
+        res.status(200).send(data);
+    } catch (error) {
+        console.log('errror', error);
+        res.status(500).send(error.toString());
+    }
+});
+
+
+//retrieving data from database (with todo/:id)
+
+
+app.get('/todos/:id', async(req, res) => {
+    try {
+        const data = await Todo.find({id: req.params.id});
+
+        res.status(200).send(data);
+    } catch (error) {
+        console.log('errror', error);
+        res.status(500).send(error.toString());
+    }
+});
 
 app.get('/user/:id', async(req, res) => {
     try {
@@ -53,4 +100,11 @@ app.get('/user/:id', async(req, res) => {
     }
 });
 
-app.listen(4001);
+// app.listen(4001);
+
+mongoose.connect (
+    'mongodb+srv://niharika:2OFL5PrOw2ByKnwD@cluster0.ur84y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+).then(result => {
+    app.listen(5001);
+})
+.catch(err => console.log(err));
